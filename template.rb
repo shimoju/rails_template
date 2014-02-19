f9f5f04738f729451b4f64873dac9598817ac829
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2014-02-19 15:12:07 +0900

    コメントを追加

--- template.rb
+++ template.rb
@@ -1,16 +1,21 @@
+# Gems
 gem_group :development, :test do
   gem 'rspec-rails'
   gem 'factory_girl_rails'
 end
 
+# Run bundle
 run 'bundle install --without production'
 
+# RSpec
 generate 'rspec:install'
 remove_dir 'test'
 
+# README
 remove_file 'README.rdoc'
 create_file 'README.md'
 
+# Git
 git :init
 git add: '.'
 git commit: %Q{ -m 'First Commit' }
