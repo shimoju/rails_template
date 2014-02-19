81771f4f2312fe3fef8ad1e7c20148494dccf7eb
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2014-02-19 15:05:08 +0900

    RSpecを追加

--- template.rb
+++ template.rb
@@ -1,4 +1,11 @@
+gem_group :development, :test do
+  gem 'rspec-rails'
+end
+
 run 'bundle install --without production'
+
+generate 'rspec:install'
+remove_dir 'test'
 
 remove_file 'README.rdoc'
 create_file 'README.md'
