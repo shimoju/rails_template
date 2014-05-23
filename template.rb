4db192482c604dd5581bf5f17bccd4c27090bf8b
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2014-05-23 20:06:05 +0900

    RSpec 3.0.0に更新

--- template.rb
+++ template.rb
@@ -71,8 +71,8 @@ end
 
 gem_group :development, :test do
   # Testing framework
-  gem 'rspec-rails', '~> 3.0.0.beta2'
-  gem 'rspec', '~> 3.0.0.beta2'
+  gem 'rspec-rails', '~> 3.0.0'
+  gem 'rspec', '~> 3.0.0'
   # Fixtures replacement
   gem 'factory_girl_rails'
   # Autoload .env
